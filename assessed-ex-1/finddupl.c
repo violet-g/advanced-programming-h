b8470c1cfbf207c8f29b75b8b0d06b39ce648312
Gabriela Georgieva <gabriella.georgieva17@gmail.com>
2016-10-27 20:23:17 +0100

    refactor main

--- assessed-ex-1/finddupl.c
+++ assessed-ex-1/finddupl.c
@@ -3,53 +3,55 @@
 #include "mlist.h"
 
 static void usage(void) {
-	fprintf(stderr, "usage: finddupl [-v]\n");
+  fprintf(stderr, "usage: finddupl [-v]\n");
 }
 
 int main(int argc, char *argv[]) {
-	MEntry *mep, *meq;
-	MList *ml;
-	char *p;
-	int c;
-	int varg = 0;
+  MEntry *mep, *meq;
+  MList *ml;
+  char *p;
+  int c;
+  int varg = 0;
 
-	if (argc > 2) {
-		usage();
-		return -1;
-	}
-	if (argc == 2) {
-		p = argv[1];
-		if (*p++ != '-') {
-			usage();
-			return -1;
-		}
-		while ((c = *p++) != '\0') {
-			if (c =='v' || c == 'V') {
-				varg++;
-			} else {
-				fprintf(stderr, "Illegal flag: %c\n", c);
-				usage();
-				return -1;
-			}
-		}
-	}
-	ml_verbose = varg;
+  if (argc > 2) {
+    usage();
+    return -1;
+  }
+  
+  if (argc == 2) {
+    p = argv[1];
+    if (*p++ != '-') {
+      usage();
+      return -1;
+    }
+    while ((c = *p++) != '\0') {
+      if (c =='v' || c == 'V') {
+        varg++;
+      } else {
+        fprintf(stderr, "Illegal flag: %c\n", c);
+        usage();
+        return -1;
+      }
+    }
+  }
+  ml_verbose = varg;
 
-	ml = ml_create();
-	while ((mep = me_get(stdin)) != NULL) {
-		meq = ml_lookup(ml, mep);
-		if (meq == NULL) {
-			(void) ml_add(&ml, mep);
-		} else {
-			printf("Potential duplicate\n");
-			printf("===================\n");
-			me_print(mep, stdout);
-			printf("==========\n");
-			me_print(meq, stdout);
-			printf("\n");
-			me_destroy(mep);
-		}
-	}
-	ml_destroy(ml);
-	return 0;
+  ml = ml_create();
+  while ((mep = me_get(stdin)) != NULL) {
+    meq = ml_lookup(ml, mep);
+    if (meq == NULL) {
+      (void) ml_add(&ml, mep);
+    } else {
+      printf("Potential duplicate\n");
+      printf("===================\n");
+      me_print(mep, stdout);
+      printf("==========\n");
+      me_print(meq, stdout);
+      printf("\n");
+      me_destroy(mep);
+    }
+  }
+
+  ml_destroy(ml);
+  return 0;
 }
